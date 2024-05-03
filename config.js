d25889a8942a5eed17ce90f4fe2ef8d108e18468
rio <ryzen483@gmail.com>
2024-05-03 13:49:52 +0700

    baru

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const mysql = require('mysql')
 //koneksi ke database sesuaikan dengan nama database
 const db = mysql.createConnection({
     host : 'sql6.freesqldatabase.com',
-    user : 'sql6695577',
-    password :'37JRDLJEE3',
-    database :'sql6695577'
+    user : 'sql6703636',
+    password :'28R3nGUYjG',
+    database :'sql6703636'
 })
 
 module.exports = db
--- index.js
+++ index.js
@@ -1,94 +1,115 @@
-const express = require('express')
-const app = express()
-const port = 3002
+const express = require('express');
+const app = express();
+const port = 3002;
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const response = require('./response.js');
 
-// memanggil request body-parser
-const bodyParser = require('body-parser')
+app.use(bodyParser.json());
 
-// memanggil file config.js
-const db = require('./config.js')
-
-// memanggil file requst.js
-const response = require('./request.js')
-
-//menggunakan body-parser
-app.use(bodyParser.json())
-
-// route get mahasiswa semua data mahasiswa
-app.get('/mahasiswa', (req,res)=>{
-    const sql = 'SELECT * FROM tb_mahasiswa'
-    db.query(sql,(error, result)=>{
-      response(200,result,'data mahasiswa',res)
-    })
-  })
-
-
-  // route get mahasiswa dengan pemanggilan npm
-  app.get('/mahasiswa/:npm', (req,res)=>{
-    const npm = req.params.npm
-    const sql =  `SELECT * FROM tb_mahasiswa where npm ='${npm}'`
-    db.query(sql, (err,result)=>{
-      if(err) throw err
-      response(200,result,"get detail mahasiswa", res)
-    })
-  })
-
-// route post mahasiswa untuk menambahkan data mahasiswa
-app.post('/mahasiswa', (req, res)=>{
-  const { nama, npm, alamat } = req.body
-  const sql = `INSERT INTO tb_mahasiswa (nama, npm, alamat) VALUES ('${nama}', '${npm}', '${alamat}');`
-
-  db.query(sql,(error, Fields)=>{
-    if (error) response(500, 'invalid',  `${nama} degan npm ${npm} sudah ditambahkan`, res)
-    if (Fields?.affectedRows){
-      const data={
-        isSucces: Fields.affectedRows,
-        id:Fields.insertId,
-      }
-      response(200, data,"Data Berhasil Disimpan", res)
-    }
-  })
-})
-
-//update data
-app.put("/mahasiswa", (req, res) => {
-  const { nama, npm, alamat } = req.body;
-  const sql = `UPDATE tb_mahasiswa SET nama='${nama}', npm='${npm}', alamat='${alamat}' WHERE npm='${npm}'`;
-
-  db.query(sql, (err, fields) => {
-    if (err) response(500, "invalid", `error`, res);
-    if (fields?.affectedRows) {
-      const data = {
-        isSuccess: fields.affectedRows,
-        message: fields.message,
-      };
-
-      response(200, data, "Data Berhasil Diupdate", res);
-    } else {
-      response(404, "not found", "error", res);
-    }
-  });
+// Membuat koneksi ke database
+const db = mysql.createConnection({
+  host : 'sql6.freesqldatabase.com',
+  user : 'sql6703636',
+  password :'28R3nGUYjG',
+  database :'sql6703636'
 });
 
-//detelete dataa
-app.delete("/mahasiswa", (req, res) => {
-  const { npm } = req.body;
-  const sql = `DELETE FROM tb_mahasiswa where npm ='${npm}'`;
-  db.query(sql, (err, fields) => {
-    if (err) response(500, "invalid", "error", res);
-    if (fields?.affectedRows) {
-      const data = {
-        isSuccess: fields.affectedRows,
-        message: fields.message,
-      };
-
-      response(200, data, "data Berhasil dihapus", res);
-    } else {
-      response(404, "not found", "error", res);
+// Menghubungkan ke database
+db.connect((err) => {
+    if (err) {
+        throw err;
     }
-  });
+    console.log('Terhubung ke database MySQL');
+});
+
+// GET endpoint untuk mendapatkan data buku
+app.get('/buku', (req, res) => {
+    const sql = 'SELECT * FROM tb_buku';
+    db.query(sql, (error, result) => {
+        if (error) {
+            response(500, 'error', 'Gagal mengambil data buku dari database', res);
+        } else {
+            response(200, result, 'Data buku berhasil diambil dari database', res);
+        }
+    });
+});
+
+// GET endpoint untuk mendapatkan data kategori
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM tb_kategori';
+    db.query(sql, (error, result) => {
+        if (error) {
+            response(500, 'error', 'Gagal mengambil data kategori dari database', res);
+        } else {
+            response(200, result, 'Data kategori berhasil diambil dari database', res);
+        }
+    });
+});
+
+// GET endpoint untuk mendapatkan data pembeli
+app.get('/pembeli', (req, res) => {
+    const sql = 'SELECT * FROM tb_pembeli';
+    db.query(sql, (error, result) => {
+        if (error) {
+            response(500, 'error', 'Gagal mengambil data pembeli dari database', res);
+        } else {
+            response(200, result, 'Data pembeli berhasil diambil dari database', res);
+        }
+    });
+});
+
+// GET endpoint untuk mendapatkan data transaksi
+app.get('/transaksi', (req, res) => {
+    const sql = 'SELECT * FROM tb_transaksi';
+    db.query(sql, (error, result) => {
+        if (error) {
+            response(500, 'error', 'Gagal mengambil data transaksi dari database', res);
+        } else {
+            response(200, result, 'Data transaksi berhasil diambil dari database', res);
+        }
+    });
+});
+
+// POST endpoint untuk menambah data buku
+app.post('/buku', (req, res) => {
+    const { judul, penulis, tahun_terbit, harga, stok, id_kategori } = req.body;
+    const sql = `INSERT INTO tb_buku (judul, penulis, tahun_terbit, harga, stok, id_kategori) VALUES (?, ?, ?, ?, ?, ?)`;
+    db.query(sql, [judul, penulis, tahun_terbit, harga, stok, id_kategori], (error, result) => {
+        if (error) {
+            response(500, 'error', 'Gagal menambahkan data buku ke database', res);
+        } else {
+            response(200, result, 'Data buku berhasil ditambahkan ke database', res);
+        }
+    });
+});
+
+// POST endpoint untuk menambah data pembeli
+app.post('/pembeli', (req, res) => {
+    const { nama, alamat, email, nomor_telepon } = req.body;
+    const sql = `INSERT INTO tb_pembeli (nama, alamat, email, nomor_telepon) VALUES (?, ?, ?, ?)`;
+    db.query(sql, [nama, alamat, email, nomor_telepon], (error, result) => {
+        if (error) {
+            response(500, 'error', 'Gagal menambahkan data pembeli ke database', res);
+        } else {
+            response(200, result, 'Data pembeli berhasil ditambahkan ke database', res);
+        }
+    });
+});
+
+// POST endpoint untuk menambah data transaksi
+app.post('/transaksi', (req, res) => {
+    const { id_pembeli, id_buku, jumlah, tanggal } = req.body;
+    const sql = `INSERT INTO tb_transaksi (id_pembeli, id_buku, jumlah, tanggal) VALUES (?, ?, ?, ?)`;
+    db.query(sql, [id_pembeli, id_buku, jumlah, tanggal], (error, result) => {
+        if (error) {
+            response(500, 'error', 'Gagal menambahkan data transaksi ke database', res);
+        } else {
+            response(200, result, 'Data transaksi berhasil ditambahkan ke database', res);
+        }
+    });
 });
 
 app.listen(port, () => {
-    console.log(`Runing in port ${port}`)
-})
+    console.log(`Running on port http://localhost:${port}`);
+});
